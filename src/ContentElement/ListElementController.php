c27e4517deadfd4272dec34c60d1b1ba84df9f1f
Stefan Lindecke <github.com@chektrion.de>
2025-02-11 12:20:08 +0100

    Update auf Twig

--- src/ContentElement/ListElementController.php
+++ src/ContentElement/ListElementController.php
@@ -15,19 +15,31 @@ use Contao\CoreBundle\DependencyInjection\Attribute\AsContentElement;
 use Contao\Template;
 use lindesbs\minkorrekt\Entity\PodcastEpisode;
 use lindesbs\minkorrekt\FormType\ListeFilterForm;
+use lindesbs\minkorrekt\Repository\PodcastEpisodeRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-#[AsContentElement(category: 'texts')]
+#[AsContentElement(category: 'texts', template: 'mi_list_element', type: 'minkorrekt_liste')]
 class ListElementController extends AbstractContentElementController
 {
+
+
+    public function __construct(
+        private readonly PodcastEpisodeRepository $podcastEpisodeRepository
+    )
+    {
+    }
+
     protected function getResponse(Template $template, ContentModel $model, Request $request): Response
     {
 
         $task = new PodcastEpisode();
         $form = $this->createForm(ListeFilterForm::class, $task);
 
-        $template->form = $form;
+        $template->form = $form->createView();
+
+        $template->data = $this->podcastEpisodeRepository->findAll();
+
 
         return $template->getResponse();
     }
--- src/FormType/ListeFilterForm.php
+++ src/FormType/ListeFilterForm.php
@@ -7,6 +7,7 @@ namespace lindesbs\minkorrekt\FormType;
 use lindesbs\minkorrekt\Entity\PodcastEpisode;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,11 +18,28 @@ class ListeFilterForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class)
-            ->add('description', TextType::class)
-            ->add('episode', DateType::class)
-            ->add('save', SubmitType::class)
-        ;
+            ->add('title', TextType::class, [
+                'required' => false,
+                'label' => false,
+                'attr' => array(
+                    'placeholder' => 'Titel'
+                )
+            ])
+            ->add('description', TextType::class, [
+                'required' => false,
+                'label' => false,
+                'attr' => array(
+                    'placeholder' => 'Beschreibung'
+                )
+            ])
+            ->add('episode', IntegerType::class, [
+                'required' => false,
+                'label' => false,
+                'attr' => array(
+                    'placeholder' => 'Episodennummer'
+                )
+            ])
+            ->add('Suchen', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
